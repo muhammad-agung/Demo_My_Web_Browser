aab65b4f47619ea09db568b884621eb82a5eebe2
MUHAMMAD AGUNG RAMADHAN RP <18004536@myrp.edu.sg>
2020-08-17 11:08:47 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/id18004536/demomywebbrowser/MainActivity.java
@@ -0,0 +1,42 @@
+package sg.edu.rp.c346.id18004536.demomywebbrowser;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnLoadURL;
+    WebView wvMyPage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        btnLoadURL = findViewById(R.id.buttonLoad);
+        wvMyPage = findViewById(R.id.webViewMyPage);
+
+        wvMyPage.setWebViewClient(new WebViewClient());
+        wvMyPage.getSettings().setJavaScriptEnabled(true);
+        wvMyPage.getSettings().setAllowFileAccess(false);
+        wvMyPage.getSettings().setBuiltInZoomControls(true);
+
+        btnLoadURL.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String url = "https://www.grab.com/sg/";
+
+                wvMyPage.loadUrl(url);
+            }
+        });
+
+
+    }
+}
